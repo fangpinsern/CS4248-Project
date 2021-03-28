bf01c810d41a4fe9ba14275a5ffcbf0b4840ea4e
branson <ngbranson567@gmail.com>
2021-03-28 15:09:35 +0800

    update constants to look for new data file

--- proj/constants.py
+++ proj/constants.py
@@ -2,7 +2,6 @@ import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # This is your Project Root
 DATA_DIR = os.path.join(ROOT_DIR, "..", "data")
-JSON_DIR = os.path.join(ROOT_DIR, "data")
-JSON_FILES = os.listdir(JSON_DIR)
+JSON_FILE = os.path.join(ROOT_DIR, "News_Category_Dataset_v2.json")
 LOG_DIR = os.path.join(ROOT_DIR, "logs")
 WEIGHTS_DIR = os.path.join(ROOT_DIR, "model_weights")
