100e10d87768b917f78c17462f58ec1e172686d0
branson <ngbranson567@gmail.com>
2021-04-01 10:09:00 +0800

    add tokenizer to dataset

--- proj/constants.py
+++ proj/constants.py
@@ -5,7 +5,7 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # This is your Project Ro
 DATA_DIR = os.path.join(ROOT_DIR, "..", "data")
 JSON_FILE = os.path.join(DATA_DIR, "News_Category_Dataset_v2.json")
 DF_FILE = os.path.join(DATA_DIR, "subsetNews.csv")
-TRAIN_TEST_SPLIT_FILE = os.path.join(DATA_DIR, 'train_test_split_dataset.csv')
+TRAIN_TEST_SPLIT_FILE = os.path.join(DATA_DIR, "train_test_split_dataset.csv")
 LOG_DIR = os.path.join(ROOT_DIR, "logs")
 WEIGHTS_DIR = os.path.join(ROOT_DIR, "model_weights")
 EMBEDDINGS_DIR = os.path.join(DATA_DIR, "embeddings")
@@ -18,6 +18,20 @@ SEED = 42
 # Data related constants
 X_COL = "headline"
 Y_COL = "category"
+
+CATEGORY_DICT = {
+    0: "CRIME",
+    1: "RELIGION",
+    2: "TECH",
+    3: "MONEY",
+    4: "FOOD & DRINK",
+    5: "SPORTS",
+    6: "TRAVEL",
+    7: "WOMEN",
+    8: "STYLE",
+    9: "ENTERTAINMENT",
+}
+
 CATEGORY_SUBSET = [
     "CRIME",
     "RELIGION",
--- proj/data/data.py
+++ proj/data/data.py
@@ -1,6 +1,5 @@
 import torch
 from torch.utils.data import Dataset, DataLoader, WeightedRandomSampler
-import emoji
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
@@ -45,49 +44,11 @@ def to_dataloader(ds, bs=64, sampler=None):
     return dl
 
 
-class TransformerDataset(Dataset):
-    """
-    converts text to tensors based on tokenizer provided
-    truncates tokens to max length provided
-    Do note the max lengths required per transformer
-    """
-
-    def __init__(self, tokenizer, df, maxLength=512):
-        self.tokenizer = tokenizer
-        self.maxLength = maxLength
-        self.df = df
-
-    def __len__(self):
-        return len(self.df)
-
-    def tokenize(self, text):
-        text = emoji.demojize(text, use_aliases=True)
-        # Handles emoji parsing that has :emoji_one: syntax
-        text = text.replace(r"[_:]", " ")
-        return self.tokenizer.encode_plus(
-            text,
-            return_tensors="pt",
-            max_length=self.maxLength,
-            truncation=True,
-            padding="max_length",
-        )
-
-    def __getitem__(self, idx):
-        # The T5 model requires summarize token since it is a multi purpose transformer
-        text = self.tokenize("summarize: " + self.df.loc[idx, "tracks"])
-        target = self.tokenize(self.df.loc[idx, "name"])
-        return (
-            text["input_ids"].flatten(),
-            text["attention_mask"].flatten(),
-            target["input_ids"].flatten(),
-            target["attention_mask"].flatten(),
-        )
-
-
 class NewsDataset(Dataset):
-    def __init__(self, df):
+    def __init__(self, df, tokenizer=None):
         self.df = df
         self.glove = torchtext.vocab.GloVe(name="6B", dim=50)
+        self.tokenizer = tokenizer
 
     def __len__(self):
         return len(self.df)
@@ -102,11 +63,24 @@ class NewsDataset(Dataset):
         return tokens
 
     def labels(self):
-        return torch.tensor(self.df[Y_COL].apply(lambda d: CATEGORY_SUBSET.index(d)).values)
+        return torch.tensor(
+            self.df[Y_COL].apply(lambda d: CATEGORY_SUBSET.index(d)).values
+        )
 
     def __getitem__(self, idx):
-        text = self.df.iloc[idx][X_COL]
         label = torch.tensor(CATEGORY_SUBSET.index(self.df.iloc[idx][Y_COL]))
+        text = self.df.iloc[idx][X_COL]
+
+        if self.tokenizer is not None:
+            tokens = self.tokenizer.encode(
+                text,
+                return_tensors="pt",
+                max_length=self.maxLength,
+                truncation=True,
+                padding="max_length",
+            )
+            return tokens, label
+
         tokens = self.tokenize(text)
         number_to_pad = MAX_INPUT_LENGTH - len(tokens)
 
