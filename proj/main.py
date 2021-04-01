fdf6e399187891c0b0823ef4711e970dc3f7d9f7
branson <ngbranson567@gmail.com>
2021-04-01 20:30:17 +0800

    some light refactor

--- proj/main.py
+++ proj/main.py
@@ -45,12 +45,13 @@ class Trainer:
         self.loss = all_loss[hp["loss"]]
         self.epochs = hp["epochs"]
         self.model.to(self.device)
-        self.writer = SummaryWriter(os.path.join(LOG_DIR, exp_name, model_name))
+        self.writer = SummaryWriter(
+            os.path.join(LOG_DIR, exp_name, model_name))
         self.exp_name = exp_name
         self.hp = hp
         self.metrics = {}
         for p in ["train", "val", "test"]:
-            self.metrics[p] = [[], []]
+            self.metrics[p] = [[], [], []]
         self.dls = dls
         self.steps = [0] * 3
         self.batch_size = dls[0].batch_size
@@ -62,9 +63,9 @@ class Trainer:
         #     {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
         #     {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
         # ]
-        # optimizer = AdamW(optimizer_grouped_parameters, lr=1e-5)
+        # optimizer = AdamW(optimizer_grouped_parameters, lr=hp["lr"])
         if hp["opt"] == "ADAM":
-            self.opt = opt(params=parameters, lr=hp["lr"], weight_decay=hp["wd"])
+            self.opt = opt(params=parameters, lr=hp["lr"])
         else:
             self.opt = opt(
                 params=parameters,
@@ -84,6 +85,7 @@ class Trainer:
     def anEpoch(self, phaseIndex, phaseName):
         losses = []
         acc_count = 0
+        allOutput, allLabels = [], []
         # we use tqdm to provide visual feedback on training stage
         for xb, yb in tqdm(self.dls[phaseIndex], total=len(self.dls[phaseIndex])):
             if self.isTransformer:
@@ -101,23 +103,34 @@ class Trainer:
                 yb = yb.to(self.device)  # BATCH_SIZE, 1
                 output = self.model(xb)  # BATCH_SIZE, 3
                 loss = self.loss(output, yb)
+            allOutput.append(output)
+            allLabels.append(yb)
 
             acc_count += accuracy(output, yb)
             losses.append(loss)
             self.steps[0] += 1
-            self._log("{}_loss".format(phaseName), loss, self.steps[phaseIndex])
-
+            self._log("{}_loss".format(phaseName),
+                      loss, self.steps[phaseIndex])
+            # remove from device
+            xb.detach().cpu()
+            yb.detach().cpu()
             if phaseIndex == 0:
                 self.opt.zero_grad()
                 loss.backward()  # calculates gradient descent
                 self.opt.step()  # updates model parameters
+        allOutput = torch.cat(allOutput)
+        allLabels = torch.cat(allLabels)
+        f1Score = skMetrics.f1_score(allLabels, allOutput, average="macro")
         losses = torch.stack(losses)
         epoch_loss = losses.mean().item()
         epoch_acc = acc_count / len(self.dls[phaseIndex]) / self.batch_size
         self.metrics[phaseName][0].append(epoch_loss)
         self.metrics[phaseName][1].append(epoch_acc)
+        self.metrics[phaseName][2].append(f1Score)
         print(
-            "\nepoch {} info: loss:{}, acc:{}".format(phaseName, epoch_loss, epoch_acc)
+            "\nepoch {} info: loss:{}, acc:{}, f1Score:{}".format(
+                phaseName, epoch_loss, epoch_acc, f1Score
+            )
         )
 
     def one_cycle(self):
@@ -134,10 +147,10 @@ class Trainer:
         if len(self.dls) > 2 and len(self.dls[2]) > 0:
             with torch.no_grad():
                 self.model.eval()
-                self.anEpoch(1, "test")
+                self.anEpoch(2, "test")
         metrics = {}
         for i in range(3):
-            metrics.update(self.get_metrics(i))
+            metrics.update(self.getMetrics(i))
         self._write_hp(metrics)  # for comparing between experiments
 
     def freeze(self):
@@ -150,24 +163,20 @@ class Trainer:
         for p in self.model.lstm.parameters():
             p.requires_grad = False
 
-    def get_metrics(self, type):
+    def getMetrics(self, type):
         phases = ["train", "val", "test"]
-        val_loss, val_acc = min(self.metrics[phases[type]][0]), max(
-            self.metrics[phases[type]][1]
-        )
-        metric_names = [
-            f"{phases[type]}_{metricType}" for metricType in ["loss", "acc"]
-        ]
-        metric_values = [val_loss, val_acc]
-
-        metrics = dict(zip(metric_names, metric_values))
+        phase = phases[type]
+        phaseMetrics = self.metrics[phases[type]]
+        metricValues = [min(phaseMetrics[0]), max(
+            phaseMetrics[1]), max(phaseMetrics[2])]
+        metrics = {}
+        for i, metricName in enumerate(["loss", "acc", "f1score"]):
+            metricName = f"{phase}_{metricName}"
+            metrics[metricName] = metricValues[i]
         return metrics
 
     def _log(self, phase, value, i):
         self.writer.add_scalar(tag=phase, scalar_value=value, global_step=i)
-        # if writer == 0:
-        # else:
-        #     self.writer_val.add_scalar(tag=phase, scalar_value=value, global_step=i)
 
     def _write_hp(self, metrics):
         self.writer.add_hparams(self.hp, metrics)
@@ -176,22 +185,16 @@ class Trainer:
         weights_path = os.path.join(WEIGHTS_DIR, self.exp_name, pkl_name)
         sd = torch.load(weights_path)
         self.model.load_state_dict(sd)
-        if num_classes is not None and family is not None:
-            if family == "densenet":
-                temp = self.model.classifier.in_features
-                self.model.classifier = torch.nn.Linear(temp, num_classes)
-            else:
-                temp = self.model.fc.in_features
-                self.model.fc = torch.nn.Linear(temp, num_classes)
         self.model.to(self.device)
 
     def _save_weights(self):
-        best_val = min(self.metrics["val"][0])
-        if self.metrics["val"][0][-1] == best_val:
+        bestF1Score = max(self.metrics["val"][-1])
+        if self.metrics["val"][-1][-1] == bestF1Score:
             weights_path = os.path.join(
                 WEIGHTS_DIR, self.exp_name, self.model_name + ".pkl"
             )
-            os.makedirs(os.path.join(WEIGHTS_DIR, self.exp_name), exist_ok=True)
+            os.makedirs(os.path.join(
+                WEIGHTS_DIR, self.exp_name), exist_ok=True)
             self.model.cpu()
             state = self.model.state_dict()
             torch.save(state, weights_path)  # open(pkl), compress
